e2178c6caadc60b4b136a97e27e4a1e24fd1b094
Aditya Bansal <bansaladitya3009@gmail.com>
2024-12-03 20:21:22 +0530

    I am gonna die alone

--- workspaceserver/apps/main/index.js
+++ workspaceserver/apps/main/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+import client from "@repo/db/client"
+
 var cors = require('cors')
 const app =express();
 const port=5000 || process.env.PORT;
-// app.get('/',(req,res)=>{
-//     res.send();
-// })
+app.get('/',(req,res)=>{
+    res.send("hello");
+})
 app.use(cors())
 app.use(express.json())
+app.use('/api/main',require('./routes/Main/main'))
 app.listen(port, ()=>{
     console.log(`listening`);
 })
---
+++ workspaceserver/apps/main/types/index.js
@@ -0,0 +1,7 @@
+import z from "zod"
+
+const SignupSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8),
+    type: z.enum(["user","admin"])
+})
